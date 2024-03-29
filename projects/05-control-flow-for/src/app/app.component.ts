4a47492031cd4fdca0043965f7238595946d91ac
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-03-29 16:36:47 +0300

    Merge remote-tracking branch 'origin/master'

--- projects/05-control-flow-for/src/app/app.component.ts
+++ projects/05-control-flow-for/src/app/app.component.ts
@@ -6,31 +6,36 @@ import { Component } from '@angular/core';
   template: `
     <section class="container">
       <!-- This article element represents and entire listing -->
+      @for (cardLists of carList; track cardLists.id) {
+
       <article class="listing">
         <div class="image-parent">
           <img class="product-image" src="https://placehold.co/100x100" />
         </div>
         <section class="details">
-          <p class="title"><!-- car make and model--></p>
+          <p class="title">{{ cardLists.make }} {{ cardLists.model }}</p>
           <hr />
           <p class="detail">
             <span>Year</span>
-            <span><!-- year --></span>
+            <span>{{ cardLists.year }}</span>
           </p>
           <div class="detail">
             <span>Transmission</span>
-            <span><!-- transmission --></span>
+            <span>{{ cardLists.transmission }}></span>
           </div>
           <p class="detail">
             <span>Mileage</span>
-            <span><!-- miles --></span>
+            <span>{{ cardLists.miles }}</span>
           </p>
           <p class="detail">
             <span>Price</span>
-            <span><!-- price --></span>
+            <span>{{ cardLists.price }}</span>
           </p>
         </section>
       </article>
+      } @empty {
+        <p>No list available</p>
+      }
     </section>
   `,
   styleUrl: 'app.component.css',
@@ -38,6 +43,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   carList = [
     {
+      id: 1,
       make: 'Foyoda',
       model: 'Famery',
       miles: 54354,
@@ -46,6 +52,7 @@ export class AppComponent {
       transmission: 'Automatic',
     },
     {
+      id: 2,
       make: 'Ronda',
       model: 'Disaccord',
       miles: 100000,
@@ -54,6 +61,7 @@ export class AppComponent {
       transmission: 'Automatic',
     },
     {
+      id: 3,
       make: 'Specific Motors',
       model: 'Spoke',
       miles: 100000,
@@ -62,6 +70,7 @@ export class AppComponent {
       transmission: 'Automatic',
     },
     {
+      id: 4,
       make: 'Fjord',
       model: 'Pocus',
       miles: 1,
