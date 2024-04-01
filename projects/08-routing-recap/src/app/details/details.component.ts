7f23531c1b024906b9ed7e72f5d61ae8eeba6e0d
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-04-01 03:43:23 +0300

    routing-recap

--- projects/08-routing-recap/src/app/details/details.component.ts
+++ projects/08-routing-recap/src/app/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,16 +7,22 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <section>
-      <p>Title</p>
+      <p>{{ this.productList[productId].title }}</p>
       <ul>
-        <li>Price</li>
-        <li>Description</li>
+        <li>{{ this.productList[productId].price }}</li>
+        <li>{{ this.productList[productId].description }}</li>
       </ul>
     </section>
   `,
   styles: ``,
 })
 export class DetailsComponent {
+
+  productId = -1;
+  @Input() set id(value: number) {
+    this.productId = value;
+  }
+
   productList = [
     {
       title: 'Product 1',
